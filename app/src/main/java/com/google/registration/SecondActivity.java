536730bfb71c455526014df47630abc42db2e381
AhmedRaafat2 <chemist.ahmedraafat@gmail.com>
2018-12-21 17:52:17 +0200

    3rdUpdate

--- app/src/main/java/com/google/registration/SecondActivity.java
+++ app/src/main/java/com/google/registration/SecondActivity.java
@@ -1,11 +1,8 @@
 package com.google.registration;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -13,7 +10,6 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
 
 
     }
